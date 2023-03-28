d3b3ac8db334dbbb3e72ab4dad2a89fbdb97e0d3
ArttieF <arttief@gmail.com>
2023-03-28 17:33:59 -0500

    Curing cancer with every commit

--- codeSlash.py
+++ codeSlash.py
@@ -4,27 +4,27 @@ from src import tGame as tg
 #Pre-Game Setup/Variable(s) below here
 #________________________________________________________________
 #Load files into variables lib### for libraries and file### for other files.
-libCore = tg.fromJson(r"colonSlash\json\core.json")
-libRaces = tg.fromJson(r"colonSlash\json\races.json")
-libISC = tg.fromJson(r"colonSlash\json\isc.json")
-libInfo = tg.fromJson(r"colonSlash\json\info.json")
-libSettings = tg.fromJson(r"colonSlash\settings.json")
+libCore = tg.fromJson(r".\json\core.json")
+libRaces = tg.fromJson(r".\json\races.json")
+libISC = tg.fromJson(r".\json\isc.json")
+libInfo = tg.fromJson(r".\json\info.json")
 #Assign contained variables from libFile to local variables.
-screenWidth = libSettings.get('screenWidth')
 menuMain = libCore.get('menuMain')
 gameName = libInfo.get('gameName')
 gameVersion = libInfo.get('version')
 title = f"{gameName} v{gameVersion}"
-ch = libSettings.get('inputChar')
 
 def main():
-    # character creation
+    # Main menu
     hold = 1
     while hold == 1:
+        fileSettings = tg.fromJson(r"settings.json")
+        inputChar = fileSettings.get('inputChar')
+        screenWidth = fileSettings.get('screenWidth')
         tg.clear
         tg.menu(libCore,title,screenWidth,option="menuMain")
-        inputWidth = int(screenWidth/2 - (len(ch) * 2.5))
-        chi = input(ch.rjust(inputWidth))
+        inputWidth = (int(screenWidth)/2 - (len(inputChar) * 2.5))
+        chi = input(inputChar.rjust(int(inputWidth)))
         if chi.lower() in ["n", "new", "newgame", "new game"]:
             hold = 0
         if chi.lower() in ["l", "load"]:
@@ -32,14 +32,13 @@ def main():
             tg.wait(2)
             hold = 1
         if chi.lower() in ["s", "settings", "setting", "set"]:
-            print("No settings yet.")
-            tg.settings()
-            tg.wait(2)
+            tg.settings(fileSettings)
             hold = 1
         if chi.lower() in ["q", "quit", "exit", "exit game"]:
             print("Exiting game...")
             tg.wait(2)
             exit()
     tg.wait(3)
+    ##Character creation starts here.
 
 main()
