d37f357a8f945a33661e1b1608c0f7da1643855e
ArttieF <arttief@gmail.com>
2023-03-30 19:51:23 -0500

    v0.1.17a

--- colonSlash.py
+++ colonSlash.py
@@ -1,8 +1,7 @@
 #import libraries below here
 #________________________________________________________________
 from src import tGame as tg
-import pathlib
-import os
+from os import rename, system, remove
 #Pre-Game Setup/Variable(s) below here
 #________________________________________________________________
 #Load files into variables lib### for libraries and file### for other files.
@@ -11,7 +10,9 @@ libRaces = tg.fromJson(r".\json\races.json")
 libISC = tg.fromJson(r".\json\isc.json")
 libInfo = tg.fromJson(r".\json\info.json")
 #Assign contained variables from libFile to local variables.
-menuMain = libCore.get('menuMain')
+menuMainA = libCore.get('menuMainA')
+menuMainB = libCore.get('menuMainB')
+menuGame = libCore.get('menuGame')
 gameName = libInfo.get('gameName')
 gameVersion = libInfo.get('version')
 title = f"{gameName} v{gameVersion}"
@@ -20,17 +21,21 @@ def main():
     # Main menu
     hold = 1
     while hold == 1:
+        if loaded == True:
+            menu = menuMainB
+        if loaded == False:
+            menu = menuMainA
         tg.clear()
         try:
             fileSettings = tg.fromJson(r"settings.json")
         except FileNotFoundError:
-            os.system("copy .\\json\\defaultSettings.json .\\ >nul")
-            os.rename(".\\defaultSettings.json", "settings.json")
+            system("copy .\\json\\defaultSettings.json .\\ >nul")
+            rename(".\\defaultSettings.json", "settings.json")
             fileSettings = tg.fromJson(r"settings.json")
         inputChar = fileSettings.get('inputChar')
         screenWidth = fileSettings.get('screenWidth')
         tg.clear
-        tg.menu(libCore,title,screenWidth,option="menuMain")
+        tg.menu(libCore,title,screenWidth,option=menu)
         inputWidth = (int(screenWidth)/2 - (len(inputChar) * 2.5))
         chi = input(inputChar.rjust(int(inputWidth)))
         if chi.lower() in ["n", "new", "newgame", "new game"]:
@@ -38,29 +43,33 @@ def main():
         if chi.lower() in ["l", "load"]:
             # print("Saving/Loading not implemented yet.")
             saveFile = tg.fromJson(r"save.json")
-            tg.clear()
-            tg.menu(libCore,title,screenWidth,option="menuMain")
-            tg.center("Select a slot to load from.",screenWidth)
-            tg.fromJson(saveFile,get="full")
-            chi2 = input(inputChar.rjust(int(inputWidth)))
-            try:
-                chi2 = int(chi2,base=10)
-            except ValueError:
-                print("Invalid slot number.")
-            if chi2 == 0:
-                save = saveFile.get('slot0')
-                print(f"Loaded {save}.")
-            if chi2 == 1:
-                save = saveFile.get('slot1')
-                print(f"Loaded {save}.")
-            if chi2 == 2:
-                save = saveFile.get('slot2')
-                print(f"Loaded {save}.")
-            if chi2 == 3:
-                save = saveFile.get('slot3')
-                print(f"Loaded {save}.")
-            tg.wait(2)
-            hold = 1
+            if saveFile.get("version") == gameVersion:
+                tg.clear()
+                tg.menu(libCore,title,screenWidth,option="menuMain")
+                tg.center("Select a slot to load from.",screenWidth)
+                tg.fromJson(saveFile,get="full")
+                chi2 = input(inputChar.rjust(int(inputWidth)))
+                try:
+                    chi2 = int(chi2,base=10)
+                except ValueError:
+                    print("Invalid slot number.")
+                if chi2 == 0:
+                    save = saveFile.get('slot0')
+                    print(f"Loaded {save}.")
+                if chi2 == 1:
+                    save = saveFile.get('slot1')
+                    print(f"Loaded {save}.")
+                if chi2 == 2:
+                    save = saveFile.get('slot2')
+                    print(f"Loaded {save}.")
+                if chi2 == 3:
+                    save = saveFile.get('slot3')
+                    print(f"Loaded {save}.")
+                tg.wait(2)
+                loaded = True
+                hold = 1
+            else:
+                remove('save.json')
         if chi.lower() in ["s", "settings", "setting", "set"]:
             tg.settings(fileSettings)
             hold = 1
